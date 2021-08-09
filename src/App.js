bc5005cb9c40dfa688aaf566b7d8be5527fb925a
unknown <pse741@naver.com>
2021-08-09 23:24:26 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+function Movie({ name }) {
+  return <h1>hi my fav movie is {name}</h1>
+}
+
+const movieLike = [
+  {
+    name: "kingkong"
+  },
+  {
+    name: "koko"
+  },
+  {
+    name: "moana"
+  },
+  {
+    name: "rapongel"
+  }
+];
+
 function App() {
   return (
     <div>
-      hi
+      <h1>Hello</h1>
+      {movieLike.map(title => <Movie name={title.name} /> )}
     </div>
   );
 }
