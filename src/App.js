4c92e53224da53d9c7feaba1e89be99706043976
unknown <pse741@naver.com>
2021-08-11 00:00:14 +0900

    day3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Movie({ name }) {
-  return <h1>hi my fav movie is {name}</h1>
-}
-
-const movieLike = [
-  {
-    name: "kingkong"
-  },
-  {
-    name: "koko"
-  },
-  {
-    name: "moana"
-  },
-  {
-    name: "rapongel"
-  }
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {movieLike.map(title => <Movie name={title.name} /> )}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add= () => {
+    this.setState(current => ({count: current.count + 1 }));
+  };
+  minus= () => {
+    this.setState(current => ({count: current.count - 1 }));
+  };
+   render(){
+     return <div>
+       <h1>The number is: {this.state.count}</h1>
+       <button onClick={this.add}>Add</button>
+       <button onClick={this.minus}>Minus</button>
+       </div>;
+   }
 }
 
 export default App;
